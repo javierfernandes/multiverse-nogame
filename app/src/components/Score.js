cd7859cb2c0ce7171f0db68186d6defe08daca4f
Javier Fernandes <javier.fernandes@gmail.com>
2018-05-22 21:27:34 -0300

    extracting style to constant

--- app/src/components/Score.js
+++ app/src/components/Score.js
@@ -1,7 +1,9 @@
 
+const style = { fontSize: '32px', fill: '#000' }
+
 export default class Score {
   create(game, getParts, getState) {
-    return this.game.add.text(16, 16, `Score: ${this.getState().score}`, { fontSize: '32px', fill: '#000' })
+    return this.game.add.text(16, 16, `Score: ${this.getState().score}`, style)
   }
   update() {
     // TODO: only set if the score changed
