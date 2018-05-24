166f78a4bd3d3174b6fa40086acba2c4557d9c05
Javier Fernandes <javier.fernandes@gmail.com>
2018-05-24 19:44:50 -0300

    cleaning up files and generalizing preload of audios

--- app/hello_world/hello_universe.spec.js
+++
@@ -1,8 +0,0 @@
-import { expect } from 'chai'
-import greet from './hello_universe'
-
-describe('hello universe', () => {
-  it('greets better than hello world', () => {
-    expect(greet()).to.equal('Hello Universe!')
-  })
-})
